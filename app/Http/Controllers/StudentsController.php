d27ec09a7ac75b844614c4b0e44ee478baa79526
unknown <professor404@gmail.com>
2022-03-09 15:09:17 +0300

    Student Information DONE

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Students;
 use Illuminate\Http\Request;
 
 class StudentsController extends Controller
 {
-    //
+    public function create(){
+        $student = Students::where('users_id', Auth::user()->id);
+        if($student){
+            return Redirect::back()->with('success', ['icon' => 'warning' ,'title' => 'انتباه', 'message' => 'لايمكنك دخول هذه الصفحة']);
+        }else{
+            return Inertia::render('Information');
+        }
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'fullname' => 'required|min:10',
+            'DOB' => 'required|date',
+            'n_lessons' => 'required|numeric',
+            'sum' => 'required|numeric',
+            'round' => 'required|numeric',
+            'branch' => 'required|numeric',
+        ]);
+
+        Students::create([
+            'fullname' => $request->fullname,
+            'DOB' => $request->dob,
+            'n_lessons' => $request->n_lessons,
+            'sum' => $request->sum,
+            'avg' => $request->sum/$request->n_lessons,
+            'round' => $request->round,
+            'branch' => $request->branch,
+            'users_id' => Auth::user()->id
+        ]);
+
+        return Redirect::route('dashboard')->with('success', ['icon' => 'success' ,'title' => 'نجحت العملية', 'message' => 'تم خزن بياناتك']);
+    }
+    
+    public function show(Students $id){
+        return Inertia::render('Show', [
+            'student' => Students::findOrFail($id)
+        ]);
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Inertia::share('success', function(){
+            return session()->get('success') ? session()->get('success') : null;
+        });
     }
 
     /**
