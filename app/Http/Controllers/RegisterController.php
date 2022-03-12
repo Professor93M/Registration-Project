50ee5ada55bd6bc61e4aa7a0e9fe6b0c6fd1d02f
unknown <professor404@gmail.com>
2022-03-13 00:00:39 +0300

    ff

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use App\Models\Register;
 use App\Models\Students;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
