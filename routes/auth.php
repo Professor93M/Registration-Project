5b7abeed44b64255502399403486a3742e3dc707
unknown <professor404@gmail.com>
2022-03-14 13:18:47 +0300

    fix logout

--- routes/auth.php
+++ routes/auth.php
@@ -17,8 +17,6 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
 });
 
 Route::middleware('auth')->group(function () {
@@ -26,12 +24,15 @@ Route::middleware('auth')->group(function () {
     Route::post('store', [StudentsController::class, 'store'])->name('store');
     Route::get('show', [StudentsController::class, 'show'])->name('show');
     Route::put('update', [StudentsController::class, 'update'])->name('update');
-
+    
     Route::get('dept', [RegisterController::class, 'create'])->name('dept');
     Route::post('save', [RegisterController::class, 'store'])->name('deptsave');
     Route::get('dshow', [RegisterController::class, 'show'])->name('dshow');
     Route::put('dupdate', [RegisterController::class, 'update'])->name('dupdate');
-
+    
     Route::get('information', [RegisterController::class, 'information'])->name('information');
+    
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
 });
 
